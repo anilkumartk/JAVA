ee9c34566b958fa6580cbd8c82a3e11cb89719b1
anilkumar_tk <anilkumartk2004@gmail.com>
2026-08-17 16:57:48 +0530

    simple example

---
+++ oops/polymorphism/method-overloading/Example1.java
@@ -0,0 +1,19 @@
+class Calc{
+   public int add(int a,int b){
+       return a+b;
+   }
+   public int add(int a,int b,int c){
+       return a+b+c;
+   }
+   public double add(double a,double b){
+       return a+b;
+   }
+}
+public class Example1 {
+    public static void main(String[] args){
+        Calc calc=new Calc();
+        System.out.println(calc.add(1,2));
+        System.out.println(calc.add(1,2,3));
+        System.out.println(calc.add(2.5,3.0));
+    }
+}
